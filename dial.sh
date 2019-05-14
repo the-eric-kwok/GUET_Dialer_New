4ffb640f17fa6847b5cebd4520f700c0b61256b7
明明 <erickwok404@gmail.com>
2019-05-14 21:45:13 +0800

    Fix double dial issue when user have both curl and wget installed.

--- dial.sh
+++ dial.sh
@@ -5,12 +5,12 @@ password=			# 你的密码
 isp=1   			# 0: 校园网 Campus Network   1: 中国电信 China Telecom   2:中国联通 China Unicom   3: 中国移动 China Mobile
 
 login() {
-  [[ $have_wget = 1 ]]&& wget "http://10.32.254.11/drcom/login?callback=dr1557825447911&DDDDD=${username}&upass=${password}&0MKKey=123456&R1=0&R3=${isp}&R6=0&para=00&v6ip=&_=15578245696520" -q -O -
+  [[ $have_wget = 1 ]]&& wget "http://10.32.254.11/drcom/login?callback=dr1557825447911&DDDDD=${username}&upass=${password}&0MKKey=123456&R1=0&R3=${isp}&R6=0&para=00&v6ip=&_=15578245696520" -q -O -; return
   [[ $have_curl = 1 ]]&& curl -d "callback=dr1557825447911&DDDDD=${username}&upass=${password}&0MKKey=123456&R1=0&R3=${isp}&R6=0&para=00&v6ip=&_=15578245696520" --url "http://10.32.254.11/drcom/login"
 }
 
 logout() {
-  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.32.254.11:801/eportal/?c=Portal&a=logout&callback=dr1557832876175&login_method=0&user_account=drcom&user_password=123&ac_logout=1&register_mode=1&wlan_user_ip=${ip_addr}&wlan_user_ipv6=&wlan_vlan_id=1&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3&_=1557832872091"
+  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.32.254.11:801/eportal/?c=Portal&a=logout&callback=dr1557832876175&login_method=0&user_account=drcom&user_password=123&ac_logout=1&register_mode=1&wlan_user_ip=${ip_addr}&wlan_user_ipv6=&wlan_vlan_id=1&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3&_=1557832872091"; return
   [[ $have_curl = 1 ]]&& curl -d "c=Portal&a=logout&callback=dr1557832876175&login_method=0&user_account=drcom&user_password=123&ac_logout=1&register_mode=1&wlan_user_ip=${ip_addr}&wlan_user_ipv6=&wlan_vlan_id=1&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3&_=1557832872091" --url "http://10.32.254.11:801/eportal/"
 }
 
