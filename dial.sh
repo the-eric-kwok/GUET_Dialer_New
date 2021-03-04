be81482780c2d2732bf1f425eb040d59e31dbd5c
EricKwok <16463312+the-eric-kwok@users.noreply.github.com>
2021-03-04 12:17:22 +0800

    Update dial.sh
    更新了拨号 IP 地址和 ISP 设置方法

--- dial.sh
+++ dial.sh
@@ -5,24 +5,31 @@
 # 2: username or password is empty #
 ####################################
 
+VERSION="1.0.0"
+
 # Config area
-username=			# 你的学号
-password=			# 你的密码
-isp=    			# 0: 校园网 Campus Network   1: 中国电信 China Telecom   2:中国联通 China Unicom   3: 中国移动 China Mobile
+username=""			# 你的学号
+password=""			# 你的密码
+
+# Uncomment below line to configure your network isp
+isp=""              # 校园网 Campus Network
+#isp="%40telecom"    # 中国电信 China Telecom
+#isp="%40unicom"     # 中国联通 China Unicom
+#isp="%40cmcc"       # 中国移动 China Mobile
 
 checkstatus() {
-  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.32.254.11/drcom/chkstatus?callback=dr1002&jsVersion=4.1&v=6500&lang=zh"; return
-  [[ $have_curl = 1 ]]&& curl -d "callback=dr1002&jsVersion=4.1&v=6500&lang=zh" --url "http://10.32.254.11/drcom/chkstatus"
+  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.0.1.5/drcom/chkstatus?callback=dr1002&jsVersion=4.1&v=6500&lang=zh"; return
+  [[ $have_curl = 1 ]]&& curl -d "callback=dr1002&jsVersion=4.1&v=6500&lang=zh" --url "http://10.0.1.5/drcom/chkstatus"
 }
 
 login() {
-  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.32.254.11/drcom/login?callback=dr1003&DDDDD=${username}&upass=${password}&0MKKey=123456&R1=0&R3=${isp}&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=4186&lang=zh"; return
-  [[ $have_curl = 1 ]]&& curl -d "callback=dr1003&DDDDD=${username}&upass=${password}&0MKKey=123456&R1=0&R3=${isp}&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=4186&lang=zh" --url "http://10.32.254.11/drcom/login"
+  [[ $have_wget = 1 ]]&& wget -q -O - "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=${username}${isp}&upass=${password}&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=4186&lang=zh"; return
+  [[ $have_curl = 1 ]]&& curl -d "callback=dr1003&DDDDD=${username}${isp}&upass=${password}&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=4186&lang=zh" --url "http://10.0.1.5/drcom/login"
 }
 
 logout() {
-  [[ $have_wget = 1 ]] && wget -q -O - "http://10.32.254.11/drcom/logout?callback=dr1005&jsVersion=4.1&v=5350&lang=zh"; return
-  [[ $have_curl = 1 ]] && curl -d "callback=dr1005&jsVersion=4.1&v=5350&lang=zh" --url "http://10.32.254.11/drcom/logout"
+  [[ $have_wget = 1 ]] && wget -q -O - "http://10.0.1.5/drcom/logout?callback=dr1005&jsVersion=4.1&v=5350&lang=zh"; return
+  [[ $have_curl = 1 ]] && curl -d "callback=dr1005&jsVersion=4.1&v=5350&lang=zh" --url "http://10.0.1.5/drcom/logout"
 }
 
 get_ip() {
@@ -33,12 +40,19 @@ help() {
   echo 'A simple script to play around with GUET network auth system.'
   echo 'GitHub page: https://github.com/the-eric-kwok/GUET_Dialer_New'
   echo ''
+  echo -e '\033[1mMODIFY THE SCRIPT TO CONFIGURE BEFORE YOU USE IT!\033[0m'
+  echo ''
   echo 'Usage: dial.sh <option>'
   echo ''
   echo 'Options:'
-  echo '  login:  login with provided info'
-  echo '  logout: you got that'
-  echo "  status: check your login status, if result=1 then you've logged in"
+  echo '  login      login with provided info'
+  echo '  logout     you got that'
+  echo "  status     check your login status, if result=1 then you've logged in"
+  echo '  version    print your script version
+}
+
+print_version() {
+  echo "Version: $VERSION"
 }
 
 which wget > /dev/null
@@ -56,7 +70,9 @@ if [ -z $username ] || [ -z $password ] || [ -z $isp ]; then
   exit 2
 fi
 
-if [ "$1" = "status" ]; then
+if [ "$1" = "version" ]; then
+  print_version
+elif [ "$1" = "status" ]; then
   checkstatus
 elif [ "$1" = "login" ]; then
   echo "Logging..."
